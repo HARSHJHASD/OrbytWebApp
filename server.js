f06648e23f38bc1b66aa42951f20a347fe211da0
harshjhaSparrow <harsh.j@sparrowrms.in>
2026-03-27 13:35:28 +0530

    chore: Increase Express body parser payload limits to 50MB.

--- server.js
+++ server.js
@@ -68,8 +68,8 @@ app.use(
 
 
 
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 const uri = process.env.MONGO_URI;
 
