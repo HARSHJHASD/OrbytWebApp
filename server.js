ef47bc230dc944511d6c4f998c6ebc58844c268a
harshjhaSparrow <harsh.j@sparrowrms.in>
2026-05-22 12:31:59 +0530

    feat: update minimum app version to 1.0.8 in configuration

--- server.js
+++ server.js
@@ -682,7 +682,7 @@ app.post('/api/cleanup', async (req, res) => {
 
 // App Version Configuration
 const APP_CONFIG = {
-  minAppVersion: "1.0.7",
+  minAppVersion: "1.0.8",
   updateUrl: "https://play.google.com/store/apps/details?id=com.orbyt.official.app"
 };
 
