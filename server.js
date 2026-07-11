2554973dd525647d7a7e93614c973404fdc9647e
harshjhaSparrow <harsh.j@sparrowrms.in>
2026-07-11 15:15:41 +0530

    chore: update minimum required application version to 1.4.2

--- server.js
+++ server.js
@@ -985,7 +985,7 @@ app.post("/api/cleanup", async (req, res) => {
 
 // App Version Configuration
 const APP_CONFIG = {
-  minAppVersion: "1.4.1",
+  minAppVersion: "1.4.2",
   updateUrl:
     "https://play.google.com/store/apps/details?id=com.orbyt.official.app",
 };
