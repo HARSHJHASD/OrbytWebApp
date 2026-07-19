848b9a429f0982772859a3908cd01d3fc13fdf58
Harsh Jha <48344178+HARSHJHASD@users.noreply.github.com>
2026-07-19 18:27:20 +0530

    Updated Code with Commits for github

--- server.js
+++ server.js
@@ -1138,7 +1138,7 @@ app.post("/api/cleanup", async (req, res) => {
 
 // App Version Configuration
 const APP_CONFIG = {
-  minAppVersion: "1.4.5",
+  minAppVersion: "1.4.6",
   updateUrl:
     "https://play.google.com/store/apps/details?id=com.orbyt.official.app",
 };
